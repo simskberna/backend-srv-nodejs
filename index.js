902535f3461fd1df6a1cfdfc626f911da5de7bfd
simskberna <simskberna@gmail.com>
2023-10-18 22:40:02 +0300

    connections updated

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient
+const cors = require('cors')
+const app = express(); 
+app.use(cors()); 
+const DATABASENAME = process.env.DATABASE
+const CONNECTION_STRING = process.env.CONNECTION_STRING
 
-const app = express();
+let database = null; 
+const PORT = process.env.PORT || 5001
 
+app.listen(PORT, () => {  
+    MongoClient.connect(CONNECTION_STRING, (error, client) => { 
+        database = client.db(DATABASENAME)  
+        if (error) {
+            console.log(error)
+        } else { 
+            console.log('Mongo DB Connection Successful')  
+            app.get('/api/v1/GetProducts', (request, response) => {  
+                
+                database.collection('productscollection').find({}).toArray((error, result) => { 
+                    response.send(result)
+                })
+            })
+            app.get('/api/v1/GetCategories', (request, response) => { 
+                database.collection('categoriescollection').find({}).toArray((error,result) => {
+                    response.send(result)
+                })
+            })
+        }
+      
+    }) 
+
+}) 
 app.get('/endpoint-1', (req, res, next) => {
     res.status(200).json({
         success: true,
@@ -20,6 +50,4 @@ app.get('/endpoint-2', (req, res, next) => {
             message: 'Endpoint  2 is working...'
         }
     })
-})
-const PORT = process.env.PORT || 5001
-app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`))
+}) 
