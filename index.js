418cb509d3247d12f00d6cabdde36cb2598afbcf
simskberna <simskberna@gmail.com>
2024-07-17 00:41:59 +0300

    fix:cors error

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 
 
 const app = express();  
-app.use(cors()); 
+app.use(cors({
+    origin: "*",
+  })); 
 const DATABASENAME = process.env.DATABASE
 const CONNECTION_STRING = process.env.CONNECTION_STRING
 
